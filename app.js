a9ba425dd2a66a64ac4bbfc025b254b226e4fc6e
Lucas <lucas.vergara@live.com>
2024-04-24 22:32:17 -0300

    :construction: Setting up the API

---
+++ app.js
@@ -0,0 +1,12 @@
+import get from './utils/getElement.js';
+const URL = "https://randomuser.me/api/";
+
+const img = get(".user-img");
+const title = get(".user-title");
+const value = get(".user-value");
+const btn = get ('.btn');
+const btns = [...document.querySelectorAll('.icon')]
+
+console.log(btns)
+
+
